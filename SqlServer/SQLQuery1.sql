fd6001c38fa2b95dfc9aa1488cc2f31b102c4187
vishwanath <vishwanathreddyaenugu@gmail.com>
2021-10-28 23:35:25 +0100

    added constraints

--- SqlServer/SQLQuery1.sql
+++ SqlServer/SQLQuery1.sql
@@ -1,3 +1,6 @@
+/*DDL*/
+
+
 CREATE DATABASE STUDENTS123;			    /*Create a database*/
 USE STUDENTS123;							/*Use database students123*/
 CREATE TABLE StudentsInfo					/*Create Table*/
@@ -31,3 +34,78 @@ INSERT INTO StudentsInfo VALUES('1','VISHU','REDDY','546486463','HEADINGLY','LEA
 TRUNCATE TABLE StudentsInfo;								   /*remove values from the table*/
 
 sp_rename 'StudentsInfo', 'InfoStudents';                      /*Rename a Table*/
+
+
+/*Contraints*/
+/*not null, unique, check, Default, Index*/
+
+
+-------------------------------------------------------------------------------------------------------------------------
+drop table StudentsInfo;
+CREATE TABLE StudentsInfo					
+(
+StudentId int,								
+StudentName varchar(8000) Not null,          -- not null (or) ALTER TABLE  StudentsInfo ALTER COLUMN PhoneNumber Not Null;
+PartnerName varchar(8000),
+PhoneNumber bigint,
+AddressOfStudent varchar(8000),
+City varchar(8000),
+Country varchar(8000)
+);
+
+
+-------------------------------------------------------------------------------------------------------------------------
+drop table StudentsInfo;
+CREATE TABLE StudentsInfo					
+(
+StudentId int,								
+StudentName varchar(8000)  ,    
+PartnerName varchar(8000),
+PhoneNumber bigint,
+AddressOfStudent varchar(8000),
+City varchar(8000),
+Country varchar(8000),
+Constraint constraint1 UNIQUE(StudentId,PhoneNumber)  -- adding unique constraints to multiple coulumn
+);
+-- (or) Alter Table StudentsInfo ADD UNIQUE(sTUDENT_ID)
+--To drop constraint
+Alter Table studentsInfo Drop Constraint constraint1;  
+
+
+-------------------------------------------------------------------------------------------------------------------------
+-- Check constraints
+drop table StudentsInfo;
+CREATE TABLE StudentsInfo					
+(
+StudentId int,								
+StudentName varchar(8000)  ,    
+PartnerName varchar(8000),
+PhoneNumber bigint,
+AddressOfStudent varchar(8000),
+City varchar(8000),
+Country varchar(8000) check (country = 'India') --check constraint
+);
+
+-- Alter table table name add constraints checkconstraintname check(country = 'India')
+
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------
+-- Default constraints
+drop table StudentsInfo;
+CREATE TABLE StudentsInfo					
+(
+StudentId int,								
+StudentName varchar(8000)  ,    
+PartnerName varchar(8000),
+PhoneNumber bigint,
+AddressOfStudent varchar(8000),
+City varchar(8000),
+Country varchar(8000) Default 'India'
+);
+
+/* 
+https://www.youtube.com/watch?v=k0S4P-a6d5w&list=PL08903FB7ACA1C2FB&index=44&t=683s
+*/
