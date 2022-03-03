3c5fc201ab0fd659a830e2dc66dd2263c3595bf9
Vishu <90732088+vishu2222@users.noreply.github.com>
2022-03-03 23:37:37 +0530

    correction

--- drive.py
+++ drive.py
@@ -1,4 +1,4 @@
 from google.colab import drive
 drive.mount("/content/drive")
 import os
-os.chdir("/content/drive/My Drive/Colab Notebooks/test.csv")
+os.chdir("/content/drive/My Drive/Colab Notebooks/new")
