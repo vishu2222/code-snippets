2aeaef88fa605d71b626c02cdc9bad8370280fa6
Vishwanath <90732088+vishu2222@users.noreply.github.com>
2022-06-14 23:02:54 +0530

    Update fileLocation.py

--- fileLocation.py
+++ fileLocation.py
@@ -5,3 +5,12 @@ print(__file__)
 print("\n")
 print(dir_path) # the path of the file from dir_path variable is located???
 
+#######################################################################
+import os 
+import glob 
+
+os.chdir(os.getcwd() + '/Data')
+path = os.getcwd() 
+files = [file for file in glob.glob(path+'/*')] 
+for i in files:
+  df = pd.read_csv(files[i])
