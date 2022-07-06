c28ca33cd3dd86689b7b2d00e6282170a7865985
Vishwanath <90732088+vishu2222@users.noreply.github.com>
2022-07-07 05:00:51 +0530

    Update template.py

--- template.py
+++ template.py
@@ -1,10 +1,15 @@
 # pandas 
 pd.set_option('display.max_rows', None)      # display max rows  
 
+###########################################################
 # subplots-1    2 adjacent plots
 fig, axs = plt.subplots(2, 1)
+fig.suptitle("suptitle")
 axs[0].plot(df['x'],df['y'])
+axs[0].title.set_text('AAAAAAAAAAAAAAAAAAAAAAA')
 axs[1].plot(df['x'],df['y'])
+axs[1].title.set_text('BBBBBBBBBBBBB')
+
 
 # subplots-2    2*2 = 4 plots
 fig, axs = plt.subplots(2, 2)
@@ -12,3 +17,4 @@ axs[0,0].plot(df['x'],df['y'])
 axs[0,1].plot(df['x'],df['y'])
 axs[1,0].plot(df['x'],df['y'])
 axs[1,1].plot(df['x'],df['y'])
+###########################################################
