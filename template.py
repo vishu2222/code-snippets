3f2ad49d837b456e5f896d2245e15158357cfbcf
Vishwanath <90732088+vishu2222@users.noreply.github.com>
2022-07-07 18:51:41 +0530

    Update template.py

--- template.py
+++ template.py
@@ -14,7 +14,7 @@ axs[1].title.set_text('BBBBBBBBBBBBB')
 # subplots-2    2*2 = 4 plots
 fig, axs = plt.subplots(2, 2)
 axs[0,0].plot(df['x'],df['y'])
-axs[0,0].ticklabel_format(useOffset=False, style='plain')   # to remove scientific notation
+axs[0,0].ticklabel_format(useOffset=False, style='plain')   # to remove scientific notation # should come in the begining of the code 
 axs[0,1].plot(df['x'],df['y'])
 axs[1,0].plot(df['x'],df['y'])
 axs[1,1].plot(df['x'],df['y'])
